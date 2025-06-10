a23f6df9768ffc4aca3b5f4160de3968b9bccec7
Mohammed Ahmed Anjarwala <mdahmedanjarwala105@gmail.com>
2025-06-10 20:09:10 +0530

    Adding the Authentication EndPoints

--- djangowork/settings.py
+++ djangowork/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "debug_toolbar",
     "django_filters",
     "rest_framework",
+    "djoser",
     "store",
     "tags",
     "likes",
@@ -145,8 +146,15 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-REST_FRAMEWORK = {
+REST_FRAMEWORK = {  # type:ignore[assignment]
     "COERCE_DECIMAL_TO_STRING": False,
+    "DEFAULT_AUTHENTICATION_CLASSES": (  # type:ignore
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
+
+SIMPLE_JWT = {
+    "AUTH_HEADER_TYPES": ("JWT",),
 }
 
 AUTH_USER_MODEL = "core.User"
