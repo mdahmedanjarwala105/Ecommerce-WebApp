5aab4f4f69cbbae7e39690388be7934639a4f406
Mohammed Ahmed Anjarwala <mdahmedanjarwala105@gmail.com>
2025-06-20 20:03:51 +0530

    Adding Signals for every new user to have the customer object get created

--- store/serializers.py
+++ store/serializers.py
@@ -204,9 +204,7 @@ class CreateOrderSerializer(serializers.ModelSerializer):
 
             cart_id = self.validated_data["cart_id"]
 
-            (customer, created) = Customer.objects.get_or_create(
-                user_id=self.context["user_id"]
-            )
+            customer = Customer.objects.get(user_id=self.context["user_id"])
             order = Order.objects.create(customer=customer)
 
             cart_item = CartItem.objects.select_related("product").filter(
