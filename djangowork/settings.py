ddf37cc3db9f19365460eee0dfaafc5e90984bde
Mohammed Ahmed Anjarwala <mdahmedanjarwala105@gmail.com>
2025-06-28 18:26:50 +0530

    Understanding CORS and implementing it

--- djangowork/settings.py
+++ djangowork/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "playground",
     "debug_toolbar",
     "django_filters",
+    "corsheaders",
     "rest_framework",
     "djoser",
     "store",
@@ -53,6 +54,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
@@ -68,6 +70,8 @@ INTERNAL_IPS = [
     "127.0.0.1",
     # ...
 ]
+
+CORS_ALLOWED_ORIGINS = ["http://localhost:8001", "http://127.0.0.1:8001"]
 
 ROOT_URLCONF = "djangowork.urls"
 
