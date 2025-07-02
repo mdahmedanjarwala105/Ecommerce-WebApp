ab5697b54190a4f86ad0a1e2a44ff174f21c52dd
Mohammed Ahmed Anjarwala <mdahmedanjarwala105@gmail.com>
2025-07-02 14:42:44 +0530

    Adding Email sender

--- djangowork/settings.py
+++ djangowork/settings.py
@@ -172,3 +172,11 @@ DJOSER = {
         "current_user": "core.serializers.UserSerializer",
     },
 }
+
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
+EMAIL_HOST_USER = ""  # os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = ""  # os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = os.environ.get("EMAIL_PORT")
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
