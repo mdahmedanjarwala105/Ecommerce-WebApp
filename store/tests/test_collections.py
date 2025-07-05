c389710ef0c044fe962c0386276d90a2f51541f7
Mohammed Ahmed Anjarwala <mdahmedanjarwala105@gmail.com>
2025-07-05 16:44:50 +0530

    Giving the access of database to the pytest

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,8 +1,10 @@
 # Every test should have 3 parts AAA - Arrange, Act, Assert
 from rest_framework.test import APIClient
 from rest_framework import status
+import pytest
 
 
+@pytest.mark.django_db
 class TestCreateCollection:
     def test_if_user_is_anonymous_returns_401(self):
 
