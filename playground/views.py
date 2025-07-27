4eccfc582767a2059aa725323249aefb6e1ce1d4
Mohammed Ahmed Anjarwala <mdahmedanjarwala105@gmail.com>
2025-07-27 22:10:59 +0530

    Caching using django

--- playground/views.py
+++ playground/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpRequest
 import requests
-from django.core.cache import cache
+from django.views.decorators.cache import cache_page
 
 
+@cache_page(5 * 60)
 def say_hello(request: HttpRequest):
 
-    key = "httpbin_result"
-    if cache.get(key) is None:
-        response = requests.get("https://httpbin.org/delay/2")
-        data = response.json()
-        cache.set(key, data)
+    response = requests.get("https://httpbin.org/delay/2")
+    data = response.json()
 
-    return render(request, "hello.html", {"name": cache.get(key)})
+    return render(request, "hello.html", {"name": data})
