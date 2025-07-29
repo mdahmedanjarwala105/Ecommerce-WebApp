fd9bf24e8fa97a5324ceda328b9c13ec9cc37749
Mohammed Ahmed Anjarwala <mdahmedanjarwala105@gmail.com>
2025-07-29 22:55:26 +0530

    Managing and cleaning Deployment and Production settings

--- djangowork/asgi.py
+++ djangowork/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangowork.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangowork.settings.dev")
 
 application = get_asgi_application()
