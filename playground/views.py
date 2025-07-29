e72173232eeae13293ec3dd733ff1edf4b604818
Mohammed Ahmed Anjarwala <mdahmedanjarwala105@gmail.com>
2025-07-29 23:13:33 +0530

    Fixing Bugs

--- playground/views.py
+++ playground/views.py
@@ -11,10 +11,13 @@ class HelloView(APIView):
 
     # @method_decorator(cache_page(5 * 60))
     def get(self, request: HttpRequest):
+
         try:
             logger.info("Calling httpbin")
             requests.get("https://httpbin.org/delay/2")
             logger.info("Received the response")
-        except request.ConnectionError:
-            logger.critical
+
+        except requests.exceptions.ConnectionError:
+            logger.critical("Connection to httpbin failed")
+
         return render(request, "hello.html", {"name": "King"})
