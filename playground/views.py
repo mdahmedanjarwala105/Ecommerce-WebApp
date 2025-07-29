bba5d4efde2840e890205e124ae185c23c5eabb6
Mohammed Ahmed Anjarwala <mdahmedanjarwala105@gmail.com>
2025-07-29 21:45:07 +0530

    Using Logger to log

--- playground/views.py
+++ playground/views.py
@@ -1,16 +1,20 @@
-from django.shortcuts import render
 from django.http import HttpRequest
-import requests
-from django.views.decorators.cache import cache_page
+from django.shortcuts import render
 from rest_framework.views import APIView
-from django.utils.decorators import method_decorator
+import requests
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class HelloView(APIView):
 
-    @method_decorator(cache_page(5 * 60))
+    # @method_decorator(cache_page(5 * 60))
     def get(self, request: HttpRequest):
-
-        requests.get("https://httpbin.org/delay/2")
-
+        try:
+            logger.info("Calling httpbin")
+            requests.get("https://httpbin.org/delay/2")
+            logger.info("Received the response")
+        except request.ConnectionError:
+            logger.critical
         return render(request, "hello.html", {"name": "King"})
