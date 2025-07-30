96d6485569736dadabd0cee80c3856c0dec1779c
Mohammed Ahmed Anjarwala <mdahmedanjarwala105@gmail.com>
2025-07-30 17:31:23 +0530

    Provisioning MySQL Database, Redis Instance and SMTP Server

--- djangowork/settings/dev.py
+++ djangowork/settings/dev.py
@@ -14,3 +14,21 @@ DATABASES = {
         "PORT": os.environ["DB_PORT"],
     }
 }
+
+CELERY_BROKER_URL = "redis://localhost:6379/1"
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/2",
+        "TIMEOUT": 10 * 60,
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+    }
+}
+
+EMAIL_HOST = os.environ["EMAIL_HOST"]
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
+EMAIL_PORT = os.environ.get("EMAIL_PORT")
